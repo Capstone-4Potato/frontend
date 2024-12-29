663e26de2238c47df592dfea8d58d07500c61120
Lee seohuyn <leesophy0805@gmail.com>
2024-12-29 18:07:42 +0900

    NotificationScreen - 알림 읽음 표시 추가, 날짜 및 시간 표시

--- lib/new_notification/notification_screen.dart
+++ lib/new_notification/notification_screen.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/colors.dart';
 import 'package:flutter_application_1/main.dart';
 import 'package:flutter_application_1/userauthmanager.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -26,7 +29,6 @@ class _NotificationScreenState extends State<NotificationScreen> {
     try {
       var url = Uri.parse('$main_url/notification');
       String? token = await getAccessToken();
-      print('$token');
 
       // api 요청 함수
       Future<http.Response> makeRequest(String token) {
@@ -91,6 +93,56 @@ class _NotificationScreenState extends State<NotificationScreen> {
     }
   }
 
+  // 생성 날짜 받아서 포맷 지정
+  String formatTimestamp(String createdAt) {
+    final now = DateTime.now();
+    final notificationTime = DateTime.parse(createdAt);
+
+    if (now.difference(notificationTime).inDays == 0) {
+      // The notification was created today
+      final difference = now.difference(notificationTime);
+      if (difference.inMinutes < 60) {
+        return '${difference.inMinutes} minutes ago';
+      } else {
+        return '${difference.inHours} hours ago';
+      }
+    } else {
+      // The notification was created on a different day
+      return DateFormat('yyyy.MM.dd').format(notificationTime);
+    }
+  }
+
+  // POST 요청 함수
+  Future<void> postNotificationRead(int id) async {
+    var url = Uri.parse('$main_url/notification/$id');
+    String? token = await getAccessToken();
+
+    try {
+      // 요청 헤더 설정
+      Map<String, String> headers = {
+        'access': token!,
+        'Content-Type': 'application/json',
+      };
+
+      // POST 요청 보내기
+      final response = await http.post(url, headers: headers);
+
+      // 상태 코드 확인 및 응답 처리
+      if (response.statusCode == 200) {
+        // 성공: JSON 파싱 후 반환
+        print("알림$id 읽음 처리 성공!");
+      } else {
+        // 실패: 에러 메시지 반환
+        throw Exception(
+            'Failed to send POST request. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      // 네트워크 또는 기타 예외 처리
+      print("Error sending POST request: $e");
+      rethrow;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -124,39 +176,100 @@ class _NotificationScreenState extends State<NotificationScreen> {
             ))
           : Padding(
               padding:
-                  const EdgeInsets.symmetric(horizontal: 14.0, vertical: 13.0),
+                  EdgeInsets.symmetric(horizontal: 14.0.w, vertical: 13.0.h),
               child: ListView.builder(
                 itemCount: notifications.length,
                 itemBuilder: ((context, index) {
                   final notification = notifications[index];
+                  final formattedTime =
+                      formatTimestamp(notification['createdAt']);
                   return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 6.0),
-                    child: ListTile(
-                      title: Text(
-                        '${notification["title"]}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
+                    padding: EdgeInsets.only(bottom: 12.0.h),
+                    child: GestureDetector(
+                      onTap: () async {
+                        // POST 알림 읽음 처리
+                        await postNotificationRead(notification['id']);
+                        setState(() {
+                          notification['unread'] = false; // 읽음 처리
+                        });
+                      },
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(8.w),
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 120.w,
+                              color: Colors.pink[100],
+                              child: Text(
+                                '${notification["title"]}',
+                                style: TextStyle(
+                                  fontSize: 16.h,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                maxLines: 1,
+                                softWrap: true,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      width: 4.w,
+                                      height: 4.h,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Color(0xFF5E5D58),
+                                      ),
+                                    ),
+                                    SizedBox(width: 5.w),
+                                    Text(
+                                      formattedTime,
+                                      style: TextStyle(
+                                        fontSize: 16.h,
+                                        fontWeight: FontWeight.w400,
+                                        color: const Color(0xFF5E5D58),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  width: 10.w,
+                                  height: 10.h,
+                                  margin: EdgeInsets.all(8.w),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: notification['unread']
+                                        ? primary
+                                        : Colors.transparent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                      ),
-                      subtitle: Text(
-                        '${notification["content"]}',
-                        style: const TextStyle(
-                          color: Color(0xFF5B5A56),
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      leading: CircleAvatar(
-                        radius: 21,
-                        backgroundColor:
-                            const Color.fromARGB(255, 255, 160, 105),
-                        child: Image.asset('assets/image/noti_character.png'),
-                      ),
-                      tileColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16) //모서리,
+                        subtitle: Text(
+                          '${notification["content"]}',
+                          style: TextStyle(
+                            color: const Color(0xFF5B5A56),
+                            fontSize: 16.h,
+                            fontWeight: FontWeight.w400,
                           ),
+                        ),
+                        leading: CircleAvatar(
+                          radius: 21.r,
+                          backgroundColor:
+                              const Color.fromARGB(255, 255, 160, 105),
+                          child: Image.asset('assets/image/noti_character.png'),
+                        ),
+                        tileColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.r) //모서리,
+                            ),
+                      ),
                     ),
                   );
                 }),
