3e387bbad4f0fca3327da169b784ed2f9c054858
Lee seohuyn <leesophy0805@gmail.com>
2025-02-15 04:25:12 +0900

    로그인 화면 UI 수정 : refactor : 로그아웃에서 loadLoginPlatform 수정 https://github.com/Capstone-4Potato/frontend/issues/1

--- lib/new/services/login_platform_manage.dart
+++ lib/new/services/login_platform_manage.dart
@@ -5,9 +5,10 @@ import 'package:flutter_application_1/new/models/login_platform.dart';
 import 'package:flutter_application_1/new/models/shared_preferences_key.dart';
 
 /// 로그인 플랫폼 불러옴
-Future<void> loadLoginPlatform() async {
+Future<LoginPlatform> loadLoginPlatform() async {
   final prefs = await SharedPreferences.getInstance();
-  prefs.getInt(SharedPreferencesKey.loginPlatform.name);
+  int index = prefs.getInt(SharedPreferencesKey.loginPlatform.name) ?? 3;
+  return LoginPlatform.values[index];
 }
 
 /// 로그인 플랫폼 저장
@@ -21,4 +22,5 @@ Future<void> saveLoginPlatform(LoginPlatform platform) async {
 Future<void> removeLoginPlatform() async {
   final prefs = await SharedPreferences.getInstance();
   await prefs.remove(SharedPreferencesKey.loginPlatform.name);
+  debugPrint("로그인 플랫폼 삭제 : $prefs ");
 }
--- lib/settings/logout/sign_out_social.dart
+++ lib/settings/logout/sign_out_social.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_application_1/new/models/login_platform.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 // 소셜로그인 로그아웃하기
@@ -6,7 +8,14 @@ class SignOutService {
   static Future<void> signOut(LoginPlatform loginPlatform) async {
     switch (loginPlatform) {
       case LoginPlatform.google:
-        // Add Google sign-out logic here
+        final GoogleSignIn googleSignIn = GoogleSignIn();
+        try {
+          await googleSignIn.signOut();
+          debugPrint('구글 로그아웃 성공');
+        } catch (error) {
+          debugPrint('구글 로그아웃 실패: $error');
+        }
+
         break;
       case LoginPlatform.kakao:
         await UserApi.instance.logout();
