0d1397d1304a5cc46b37bb0cc3cd68596af603bf
Lee seohuyn <leesophy0805@gmail.com>
2025-02-15 18:05:51 +0900

    로그인 화면 UI 수정 : refactor : 로그인 화면 간격 수정

--- lib/new/screens/login_screen.dart
+++ lib/new/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/new/models/image_path.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:flutter_application_1/new/models/colors.dart';
 import 'package:flutter_application_1/new/models/font_family.dart';
 import 'package:flutter_application_1/new/widgets/sign_in_image_button.dart';
@@ -16,90 +15,84 @@ class LoginScreen extends StatelessWidget {
     double width = MediaQuery.sizeOf(context).width;
 
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              top: 320.h,
-              child: Image.asset(
-                ImagePath.loginBgText.path,
-                width: width,
-              ),
+      body: Stack(
+        children: [
+          // 로그인 화면 배경 문구
+          Positioned(
+            left: 0,
+            top: 320.h,
+            child: Image.asset(
+              ImagePath.loginBgText.path,
+              width: width,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Spacer(flex: 5),
-                Container(
-                  width: 58.w,
-                  height: 58.h,
-                  alignment: Alignment.bottomCenter,
-                  decoration: BoxDecoration(
-                    color: AppColors.orange_001,
-                    borderRadius: BorderRadius.circular(10.r),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withValues(alpha: 0.25),
-                        spreadRadius: 0,
-                        blurRadius: 4,
-                        offset: const Offset(0, 1),
-                      ),
-                    ],
-                  ),
-                  child: Image.asset(
-                    ImagePath.loginBalbamCharacter.path,
-                    fit: BoxFit.fitWidth,
-                  ),
-                ),
-                Text(
-                  'Welcome to',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: AppColors.brown_001,
-                    fontFamily: FontFamily.bmJua.fontName,
-                    fontSize: 52.0.h,
-                    fontWeight: FontWeight.w400,
-                    wordSpacing: 1.2.w,
-                  ),
-                ),
-                Text(
-                  'Balbam\nBalbam',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: AppColors.orange_000,
-                      fontFamily: FontFamily.bmJua.fontName,
-                      fontSize: 48.0.h,
-                      fontWeight: FontWeight.w400,
-                      height: 1.2),
-                ),
-                const Spacer(flex: 5),
-                Text(
-                  'Log in or sign up to Get Started',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: AppColors.brown_001,
-                    fontFamily: FontFamily.bmJua.fontName,
-                    fontSize: 18.0.h,
-                    fontWeight: FontWeight.w400,
-                    wordSpacing: 1.1.w,
-                  ),
-                ),
-                SizedBox(height: 24.h),
-                ...[
-                  LoginPlatform.apple,
-                  LoginPlatform.kakao,
-                  LoginPlatform.google,
-                ]
-                    .map((platform) =>
-                        SignInImageButton(loginPlatform: platform))
-                    .toList(),
-                const Spacer(flex: 2),
-              ],
-            ),
-          ],
-        ),
+          ),
+
+          //버튼 및 text
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 126.h),
+              _buildLogoContainer(),
+              _buildText('Welcome to', AppColors.brown_001, 52.0.h, 1.2),
+              _buildText('Balbam\nBalbam', AppColors.orange_000, 48.0.h, 1.0),
+              SizedBox(height: 125.h),
+              _buildText('Log in or sign up to Get Started',
+                  AppColors.brown_001, 18.0.h),
+              SizedBox(height: 16.h),
+              _buildSignInButtons(),
+            ],
+          ),
+        ],
       ),
+    );
+  }
+
+  /// 로고 아이콘 박스 빌드
+  Widget _buildLogoContainer() {
+    return Container(
+      width: 58.w,
+      height: 58.h,
+      alignment: Alignment.bottomCenter,
+      decoration: BoxDecoration(
+        color: AppColors.orange_001,
+        borderRadius: BorderRadius.circular(10.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.25),
+            blurRadius: 4,
+            offset: const Offset(0, 1),
+          ),
+        ],
+      ),
+      child: Image.asset(
+        ImagePath.loginBalbamCharacter.path,
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+
+  /// 로그인 화면 중앙 text 빌드
+  Widget _buildText(String text, Color color, double fontSize,
+      [double? height]) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: color,
+        fontFamily: FontFamily.bmJua.fontName,
+        fontSize: fontSize,
+        fontWeight: FontWeight.w400,
+        height: height,
+      ),
+    );
+  }
+
+  /// 로그인 버튼 빌드
+  Widget _buildSignInButtons() {
+    return Column(
+      children: LoginPlatform.values
+          .map((platform) => SignInImageButton(loginPlatform: platform))
+          .toList(),
     );
   }
 }
