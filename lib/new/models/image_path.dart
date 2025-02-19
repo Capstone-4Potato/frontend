4472c6ca5361ee037da190fa5cf338d4d455fbb8
Lee seohuyn <leesophy0805@gmail.com>
2025-02-19 22:27:18 +0900

    푸쉬 알림 기능 추가 : feat : 로그인 화면 아이콘 수정, 앱 아이콘 수정 https://github.com/Capstone-4Potato/frontend/issues/7

--- lib/new/models/image_path.dart
+++ lib/new/models/image_path.dart
@@ -1,7 +1,7 @@
 /// 이미지 저장 경로
 enum ImagePath {
   loginBgText(path: 'assets/image/login_bg_text.png'),
-  loginBalbamCharacter(path: 'assets/image/login_balbam_character.png'),
+  loginBalbamCharacter(path: 'assets/image/login_balbam_character.svg'),
   recoverDialogBalbam(path: 'assets/image/recover_dialog_balbam.svg'),
   deleteDialogCryingBalbam(
       path: 'assets/image/delete_dialog_crying_balbam.svg');
