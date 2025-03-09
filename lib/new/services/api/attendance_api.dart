34992d942954ce80f24eae40e39da2cb5e586ab5
Lee seohuyn <leesophy0805@gmail.com>
2025-03-09 18:53:00 +0900

    API 요청 함수로 통일 : refactor :  Attendance API 요청 함수 수정 https://github.com/Capstone-4Potato/frontend/issues/18

---
+++ lib/new/services/api/attendance_api.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/new/models/api_method.dart';
+import 'package:flutter_application_1/new/services/api/api_service.dart';
+
+/// ### GET `home/attendance` : 사용자 출석 날짜 조회
+/// 사용자의 월별 출석 날짜를 조회합니다.
+Future<void> getUserAttendanceRequest({
+  required Function(Map<String, dynamic>) onDataReceived,
+}) async {
+  try {
+    await apiRequest(
+      endpoint: 'home/attendance',
+      method: ApiMethod.get.type,
+      onSuccess: (response) {
+        final data = jsonDecode(response.body);
+        onDataReceived(data);
+      },
+    );
+  } catch (e) {
+    debugPrint('취약 음소 정보 조회 실패 : $e');
+  }
+}
